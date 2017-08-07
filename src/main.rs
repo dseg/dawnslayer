82eff33145897b65de1f74356acfac54fa11d050
Daichi Shinozaki <shinozaki@zcomwallet.com>
2017-08-07 21:09:22 +0900

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,19 @@ use std::thread;
 const RPC_PORT: i32 = 58332;
 
 const NAME: &'static str = "DawnSlayer";
-const VER:  &'static str = "0.0.1";
+const VER: &'static str = "0.0.1";
 
 fn ua() -> String {
     return format!("{}/{}", NAME, VER);
 }
 
 fn handle_client(mut stream: TcpStream) {
-    thread::spawn(move || {
-        stream.write(b"Hello World\r\n").unwrap();
-    });
+    thread::spawn(move || { stream.write(b"Hello World\r\n").unwrap(); });
 }
 
-fn handle_mainnet(mut stream: TcpStream) {
-    
-}
+fn handle_mainnet(mut stream: TcpStream) {}
 
-fn main() { 
+fn main() {
     let rpc_listener = TcpListener::bind(format!("127.0.0.1:{}", RPC_PORT)).unwrap();
     println!("{} listening started, ready to accept connections.", ua());
 
@@ -32,22 +28,18 @@ fn main() {
             Ok(stream) => {
                 handle_client(stream);
             }
-            Err(e) => { 
-                /* connection failed */ 
-            }
+            Err(e) => { /* connection failed */ }
         }
     }
-    
+
     // Connect to the full node on local host, standard port.
     let mainnet_listener = TcpListener::bind("127.0.0.1:8333").unwrap();
     for stream in mainnet_listener.incoming() {
         match stream {
-	  Ok(stream) => {
-	    handle_mainnet(stream);
-	  }
-	  Err(e) => {
-                /* connection failed */ 	  
-	  }
-	}
+            Ok(stream) => {
+                handle_mainnet(stream);
+            }
+            Err(e) => { /* connection failed */ }
+        }
     }
 }
