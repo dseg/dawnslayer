bab1412af1ac3ce4fb2305800fdd934802bb9715
Daichi Shinozaki <shinozaki@zcomwallet.com>
2017-08-07 19:21:00 +0900

    Added User Agent.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,27 @@ use std::io::Write;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
+const RPC_PORT: i32 = 58332;
+
+const NAME: &'static str = "DawnSlayer";
+const VER:  &'static str = "0.1.0";
+
+fn ua() -> String {
+    return format!("{}/{}", NAME, VER);
+}
+
 fn handle_client(mut stream: TcpStream) {
      thread::spawn(move || {
         stream.write(b"Hello World\r\n").unwrap();
      });
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:54321").unwrap();
-    println!("listening started, ready to accept");
+fn main() { 
+    let rpc_listener = TcpListener::bind(format!("127.0.0.1:{}", RPC_PORT)).unwrap();
+    println!("{} listening started, ready to accept connections.", ua());
 
-    for stream in listener.incoming() {
+    // Waiting for connections.
+    for stream in rpc_listener.incoming() {
         match stream {
             Ok(stream) => {
                 handle_client(stream);
