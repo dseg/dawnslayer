75dd1efe1f89bdae8eb52fa673d203cf1937e31d
Daichi Shinozaki <shinozaki@zcomwallet.com>
2017-08-07 20:36:28 +0900

    Updated dependencies.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+//use std::str;
 
 const RPC_PORT: i32 = 58332;
 
 const NAME: &'static str = "DawnSlayer";
-const VER:  &'static str = "0.1.0";
+const VER:  &'static str = "0.0.1";
 
 fn ua() -> String {
     return format!("{}/{}", NAME, VER);
 }
 
 fn handle_client(mut stream: TcpStream) {
-     thread::spawn(move || {
+    thread::spawn(move || {
         stream.write(b"Hello World\r\n").unwrap();
-     });
+    });
+}
+
+fn handle_mainnet(mut stream: TcpStream) {
+    
 }
 
 fn main() { 
@@ -31,5 +36,18 @@ fn main() {
                 /* connection failed */ 
             }
         }
+    }
+    
+    // Connect to the full node on local host, standard port.
+    let mainnet_listener = TcpListener::bind("127.0.0.1:8333").unwrap();
+    for stream in mainnet_listener.incoming() {
+        match stream {
+	  Ok(stream) => {
+	    handle_mainnet(stream);
+	  }
+	  Err(e) => {
+                /* connection failed */ 	  
+	  }
+	}
     }
 }
