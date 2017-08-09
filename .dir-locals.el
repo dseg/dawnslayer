c9e60d6c64d9ddf917fde637c0309488bc9df3b8
Daichi Shinozaki <d.shinozaki@z.com>
2017-08-10 01:44:58 +0900

    Added dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((rust-mode . ((eval . (progn
+                        (c-set-style "BSD")
+                        (setq-default indent-tabs-mode nil)
+                        (setq-default tab-width 4)
+                        (setq c-basic-offset 4)
+                        )))))
